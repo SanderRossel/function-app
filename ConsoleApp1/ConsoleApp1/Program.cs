c2083e053c0d8b7b4b23fda2764f7448dc09c12c
Sander Rossel <sander.rossel@outlook.com>
2018-08-26 13:44:41 +0200

    Function App default template + consuming Console App
    The default VS2017 template for a Function App in .NET Standard and a consuming Console App.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                // Replace with your own URL.
+                string url = "http://localhost:7071/api/Function1";
+                var content = new StringContent("{\"name\": \"Function\"}", Encoding.UTF8, "application/json");
+                client.PostAsync(url, content).ContinueWith(async t =>
+                {
+                    var result = await t.Result.Content.ReadAsStringAsync();
+                    Console.WriteLine("The Function result is:");
+                    Console.WriteLine(result);
+                    Console.WriteLine("Press any key to exit.");
+                    Console.ReadKey();
+                }).Wait();
+            }
+        }
+    }
+}
