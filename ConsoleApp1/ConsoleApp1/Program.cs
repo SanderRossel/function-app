a2c701aa707320c071a834b51b43dc209addbc6e
Sander Rossel <sander.rossel@outlook.com>
2018-08-26 16:53:02 +0200

    Changed Function output

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,14 +4,14 @@ using System.Text;
 
 namespace ConsoleApp1
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             using (var client = new HttpClient())
             {
                 // Replace with your own URL.
-                string url = "http://localhost:7071/api/Function1";
+                string url = "https://functions2213.azurewebsites.net/api/Function1?code=HGVGvjEecLEKxZObeJahStGHkNra7UAw8hORfkC3wYGQisM9Swf42w==";
                 var content = new StringContent("{\"name\": \"Function\"}", Encoding.UTF8, "application/json");
                 client.PostAsync(url, content).ContinueWith(async t =>
                 {
--- FunctionApp/FunctionApp/Function1.cs
+++ FunctionApp/FunctionApp/Function1.cs
@@ -22,7 +22,7 @@ namespace FunctionApp
             name = name ?? data?.name;
 
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                ? (ActionResult)new OkObjectResult($"Hello and bye, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
